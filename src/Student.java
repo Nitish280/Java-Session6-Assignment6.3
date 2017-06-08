9299f7f619791814c44fd0fe76b540e568a3f4f3
Nitish280 <nsrccs@gmail.com>
2017-06-08 15:20:38 +0530

    Create Student.java

--- src/Student.java
+++ src/Student.java
@@ -5,15 +5,15 @@ public class Student {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Student s1=new Student();
-		System.out.println("print the value of id: " +s1.id);  
-		System.out.println("print the value of name: " + s1.name); 
+		Student s1=new Student();//creating an object of Student  
+		System.out.println("print the value of id: " +s1.id); //here print the value of id with the refrence of object 
+		System.out.println("print the value of name: " + s1.name); //here print the value of name with the refrence of object 
 		
-		s1.id=20;
-		s1.name="Singh";
+		s1.id=20;//here we are changing the value of id
+		s1.name="Singh";//here we are changing the value of id
 		
-		System.out.println("print the value of id: " +s1.id);  
-		System.out.println("print the value of name: " + s1.name); 
+		System.out.println("print the value of id: " +s1.id); //here print the changing value of id with the refrence of object 
+		System.out.println("print the value of name: " + s1.name); //here print the value of id with the refrence of object
 
 	}
 
